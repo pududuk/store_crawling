cd2b2f2a5d45c09f8af6a87c0a8d965e77c63788
yubin-lge <yubin.an@lge.com>
2025-07-07 13:56:21 +0900

    [feat] model change : mobile model

--- app.py
+++ app.py
@@ -5,82 +5,72 @@ backend_url = "http://34.64.137.143:8080/api/menu/ocr-response"
 from PIL import Image
 import numpy as np
 import skimage as ski
+from skimage.morphology import dilation, erosion
 from paddleocr import PaddleOCR
-import cv2
 import os
 import getpass
+import threading
 
 username = "ubuntu"
 
-ocr_kr = PaddleOCR(
-    lang="korean",
-    use_doc_orientation_classify=False,
-    use_doc_unwarping=False,
-    use_textline_orientation=False,
-    text_detection_model_dir=f'/home/{username}/.paddlex/official_models/PP-OCRv5_server_det',
-    text_detection_model_name='PP-OCRv5_server_det',
-    text_recognition_model_dir=f'/home/{username}/.paddlex/official_models/korean_PP-OCRv5_mobile_rec',
-    text_recognition_model_name='korean_PP-OCRv5_mobile_rec',
-)
-ocr_en = PaddleOCR(
-    lang="en",
-    use_doc_orientation_classify=False,
-    use_doc_unwarping=False,
-    use_textline_orientation=False,
-    text_detection_model_dir=f'/home/{username}/.paddlex/official_models/PP-OCRv5_server_det',
-    text_detection_model_name='PP-OCRv5_server_det',
-    text_recognition_model_dir=f'/home/{username}/.paddlex/official_models/PP-OCRv5_server_rec',
-    text_recognition_model_name='PP-OCRv5_server_rec',
-)
+_local = threading.local()
+
+def get_ocr():
+    if not hasattr(_local, "ocr"):
+        _local.ocr = PaddleOCR(
+            use_doc_orientation_classify=False,
+            use_doc_unwarping=False,
+            use_textline_orientation=False,
+            text_detection_model_dir=f'/home/{username}/.paddlex/official_models/PP-PP-OCRv5_mobile_det',
+            text_detection_model_name='PP-OCRv5_mobile_det',
+            text_recognition_model_dir=f'/home/{username}/.paddlex/official_models/korean_PP-OCRv5_mobile_rec',
+            text_recognition_model_name='korean_PP-OCRv5_mobile_rec',
+        )
+    return _local.ocr
 
 def ocr_ourhome(file):
+    ocr = get_ocr()
     img_pil = Image.open(file).convert("RGB")
     img = np.array(img_pil)
     img_y = np.array(img_pil.convert("L"))
 
     fit = img_y[:10, :10].mean() < 80
 
-    # 이진화
-    img_y_bin = np.where(img_y < 240, 0, 255).astype(np.uint8)
+    img_y[np.where(img_y < 240)] = 0
+    img_y[np.where(img_y > 0)] = 255
 
-    # 세로 방향 3분할
-    img_y_splits = np.array_split(img_y_bin, 3, axis=1)
+    img_y_splits = np.array_split(img_y, 3, axis=1)
     img_splits = np.array_split(img, 3, axis=1)
 
-    # 메뉴 번호 추출
+    menu_corners_in = []
+    for img_y_split in img_y_splits:
+        img_y_split = img_y_split[50:250, 200:650, None]
+        for _ in range(2):
+            img_y_split = erosion(img_y_split)[::2, ::2]
+        menu_corners_in.append(img_y_split)
+
     if fit:
         menu_corners = ["B1", "B1", "B1"]
     else:
-        menu_corners = []
-        for img_y_split in img_y_splits:
-            crop = img_y_split[:500]
-            resized = cv2.resize(crop, (crop.shape[1] // 2, crop.shape[0] // 2))
-            input_img = np.stack([resized] * 3, axis=-1)
-            result = ocr_en.predict(input_img)[0]["rec_texts"][0]
-            menu_corners.append(result)
+        menu_corners = [
+            ocr.predict(np.repeat(img_y_split, 3, axis=-1))[0]["rec_texts"][0].replace(".", ",")
+            for img_y_split in menu_corners_in
+        ]
 
-    # 메뉴 이름 추출
-    menu_names = []
-    for img_y_split in img_y_splits:
-        crop = img_y_split[800:1000]
-        resized = cv2.resize(crop, (crop.shape[1] // 2, crop.shape[0] // 2))
-        input_img = np.stack([resized] * 3, axis=-1)
-        result = ocr_kr.predict(input_img)[0]["rec_texts"][0]
-        menu_names.append(result)
+    menu_names = [
+        ocr.predict(np.repeat(dilation(img_y_split[800:1000, :, None])[::2, ::2], 3, axis=-1))[0]["rec_texts"][0]
+        for img_y_split in img_y_splits
+    ]
 
-    # 메뉴 이미지 추출
     crop_h = 475
     menu_imgs = []
 
     for i, img_y_split in enumerate(img_y_splits):
-        col_mean = img_y_split.mean(axis=0)
-        row_mean = img_y_split.mean(axis=1)
+        lb_idx_w_l = np.where(img_y_split[:, :400].mean(axis=0) > 150)[0][-1]
+        lb_idx_w_r = np.where(img_y_split[:, 400:].mean(axis=0) > 150)[0][0] + 400
+        lb_idx_h = np.where(img_y_split[600:,].mean(axis=1) > 150)[0][0] + 600
+        menu_img = img_splits[i][(lb_idx_h - crop_h) : lb_idx_h, lb_idx_w_l : lb_idx_w_r]
 
-        lb_idx_w_l = np.where(col_mean[:400] > 150)[0][-1]
-        lb_idx_w_r = np.where(col_mean[400:] > 150)[0][0] + 400
-        lb_idx_h = np.where(row_mean[600:] > 150)[0][0] + 600
-
-        menu_img = img_splits[i][(lb_idx_h - crop_h):lb_idx_h, lb_idx_w_l:lb_idx_w_r]
         buffer = BytesIO()
         Image.fromarray(menu_img).save(buffer, format="PNG")  # 압축 저장
         buffer.seek(0)
@@ -89,48 +79,45 @@ def ocr_ourhome(file):
     return menu_imgs, menu_corners, menu_names
 
 def ocr_cjfresh(file):
+    ocr = get_ocr()
     img_pil = Image.open(file).convert("RGB")
     img = np.array(img_pil)
     img_y = np.array(img_pil.convert("L"))
 
-    # 이진화
-    img_y_bin = np.where(img_y < 240, 0, 255).astype(np.uint8)
+    img_y[np.where(img_y < 240)] = 0
+    img_y[np.where(img_y > 0)] = 255
 
-    # 메뉴 번호 추출 (좌측 상단 부분 다운샘플링)
-    crop_corner = img_y_bin[:250, :500]
-    resized_corner = cv2.resize(crop_corner, (crop_corner.shape[1] // 2, crop_corner.shape[0] // 2))
-    corner_rgb = np.stack([resized_corner] * 3, axis=-1)
-    menu_corners = [ocr_en.predict(corner_rgb)[0]["rec_texts"][0]]
+    corner_img_in = img_y[60:250, 120:350, None]
+    for _ in range(2):
+        corner_img_in = erosion(corner_img_in)[::2, ::2]
+    menu_corners = [ocr.predict(np.repeat(corner_img_in, 3, axis=-1))[0]["rec_texts"][0][:2]]
 
-    # 메뉴 바운딩 좌표 계산
-    w_l = np.where(img_y_bin[:, :500].mean(axis=0) > 200)[0][-1].item()
-    w_r = np.where(img_y_bin[:, 500:].mean(axis=0) > 200)[0][0].item() + 500
-    h_t = np.where(img_y_bin[:500, :].mean(axis=1) > 200)[0][-1].item()
-    h_b = np.where(img_y_bin[500:, :].mean(axis=1) > 200)[0][0].item() + 500
+    w_l = np.where(img_y[:, :500].mean(axis=0) > 200)[0][-1].item()
+    w_r = np.where(img_y[:, 500:].mean(axis=0) > 200)[0][0].item() + 500
+    h_t = np.where(img_y[:500, :].mean(axis=1) > 200)[0][-1].item()
+    h_b = np.where(img_y[500:, :].mean(axis=1) > 200)[0][0].item() + 500
 
     # 메뉴 이미지 잘라내기
-    menu_crop = img[h_t:h_b, w_l:w_r]
-    buffer = BytesIO()
-    Image.fromarray(menu_crop).save(buffer, format="PNG")  # 압축 저장
+    menu_cropped = img[h_t:h_b, w_l:w_r]
+    buffer = BytesIO(Image.fromarray(menu_cropped))
     buffer.seek(0)
-    menu_imgs = [buffer]
+    menu_imgs = [buffer.tobytes()]
 
     # 메뉴 이름 OCR (G 채널 활용)
-    img_g = img[..., 1]
-    img_g_bin = np.where((img_g < 130) | (img_g > 150), 255, img_g).astype(np.uint8)
+    img_g = img[..., 1].copy()
+    img_g[np.where(img_g > 150)] = 255
+    img_g[np.where(img_g < 130)] = 255
 
-    text_crop = img_g_bin[400:1000, 1100:1850]
-    text_crop = ski.morphology.dilation(text_crop)
-    resized_text = cv2.resize(text_crop, (text_crop.shape[1] // 2, text_crop.shape[0] // 2))
-    text_rgb = np.stack([resized_text] * 3, axis=-1)
-    menu_names = ["".join(ocr_kr.predict(text_rgb)[0]["rec_texts"])]
+    name_img_in = dilation(img_g[400:1000, 1100:1850, None])[::2, ::2]
+    idxs = np.where(name_img_in.mean(axis=1) < 255)[0]
+    name_img_in = name_img_in[(idxs[0] - 10) : (idxs[-1] + 10)]
+    menu_names = ["".join(ocr.predict(np.repeat(name_img_in, 3, axis=-1))[0]["rec_texts"]).replace(" ", "")]
 
     return menu_imgs, menu_corners, menu_names
 
 
 ### 아래는 건들지 말기
 from flask import Flask, request, jsonify
-import threading
 import requests
 from io import BytesIO
 
@@ -144,10 +131,6 @@ def post_image(image_bytesio_list, image_meta, url):
     form_data = image_meta
     r = requests.post(url, files=files, data=form_data)
 
-    # if r.status_code == requests.codes.ok:
-    #     pass
-    # else:
-    #     print(f"Request timeout; Please check backend url : {backend_url}")
 
 def ocr_and_post(menu_urls, ocr_fn):
     # url -> image
@@ -164,8 +147,9 @@ def ocr_and_post(menu_urls, ocr_fn):
     menu_images_list = []
     menu_corners_list = []
     menu_names_list = []
-    for img in imgs:
+    for idx, img in enumerate(imgs):
         menu_images, menu_corners, menu_names = ocr_fn(img)
+        print("OCR success")
         menu_images_list.extend(menu_images)
         menu_corners_list.extend(menu_corners)
         menu_names_list.extend(menu_names)
@@ -173,6 +157,7 @@ def ocr_and_post(menu_urls, ocr_fn):
     # POST
     menu_meta = {"corners": ','.join(menu_corners_list), "names": ','.join(menu_names_list)}
     post_image(menu_images_list, menu_meta, backend_url)
+    print("POST success")
 
 @app.route('/test', methods=["POST"])
 def test():
